60a482c8c195622dd9389068991a58f28ec106e3
racechao <jiechao@jobright.ai>
2024-04-03 21:36:14 +0800

    bug report on watchOS

--- Example/SDWebImageSwiftUIDemo/ContentView.swift
+++ Example/SDWebImageSwiftUIDemo/ContentView.swift
@@ -6,8 +6,33 @@
  * file that was distributed with this source code.
  */
 
-import SwiftUI
 import SDWebImageSwiftUI
+import SwiftUI
+
+struct FastSwitchGifView: View {
+    let fileName: String
+
+    // if gifURL is null, will not play animation
+//    @State private var gifURL: URL?
+    @State private var gifURL: URL? = Bundle.main.url(forResource: "joy", withExtension: "gif")
+
+    var body: some View {
+        WebImage(url: gifURL)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+
+            .onAppear {
+                // Add delay to fix change gif
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    switchToNewGif()
+                }
+            }
+    }
+
+    func switchToNewGif() {
+        gifURL = Bundle.main.url(forResource: "dogflops", withExtension: "gif")
+    }
+}
 
 class UserSettings: ObservableObject {
     // Some environment configuration
@@ -70,24 +95,24 @@ struct ContentView2: View {
         "https://raw.githubusercontent.com/liyong03/YLGIFImage/master/YLGIFImageDemo/YLGIFImageDemo/joy.gif"
     ]
     @State var animated: Bool = false // You can change between WebImage/AnimatedImage
-    @State var imageIndex : Int = 0
+    @State var imageIndex: Int = 0
     var body: some View {
         Group {
             Text("\(animated ? "AnimatedImage" : "WebImage") - \((imageURLs[imageIndex] as NSString).lastPathComponent)")
             Spacer()
             #if os(watchOS)
-            WebImage(url:URL(string: imageURLs[imageIndex]))
-            .resizable()
-            .aspectRatio(contentMode: .fit)
+            WebImage(url: URL(string: imageURLs[imageIndex]))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             #else
             if self.animated {
-                AnimatedImage(url:URL(string: imageURLs[imageIndex]))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+                AnimatedImage(url: URL(string: imageURLs[imageIndex]))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
             } else {
-                WebImage(url:URL(string: imageURLs[imageIndex]))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+                WebImage(url: URL(string: imageURLs[imageIndex]))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
             }
             #endif
             Spacer()
@@ -109,30 +134,30 @@ struct ContentView2: View {
 
 struct ContentView: View {
     @State var imageURLs = [
-    "http://assets.sbnation.com/assets/2512203/dogflops.gif",
-    "https://raw.githubusercontent.com/liyong03/YLGIFImage/master/YLGIFImageDemo/YLGIFImageDemo/joy.gif",
-    "http://apng.onevcat.com/assets/elephant.png",
-    "http://www.ioncannon.net/wp-content/uploads/2011/06/test2.webp",
-    "http://www.ioncannon.net/wp-content/uploads/2011/06/test9.webp",
-    "http://littlesvr.ca/apng/images/SteamEngine.webp",
-    "http://littlesvr.ca/apng/images/world-cup-2014-42.webp",
-    "https://isparta.github.io/compare-webp/image/gif_webp/webp/2.webp",
-    "https://raw.githubusercontent.com/link-u/avif-sample-images/master/fox.profile0.8bpc.yuv420.avif",
-    "https://raw.githubusercontent.com/link-u/avif-sample-images/master/star-12bpc-with-alpha.avifs",
-    "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic",
-    "https://nokiatech.github.io/heif/content/image_sequences/starfield_animation.heic",
-    "https://nr-platform.s3.amazonaws.com/uploads/platform/published_extension/branding_icon/275/AmazonS3.png",
-    "https://raw.githubusercontent.com/ibireme/YYImage/master/Demo/YYImageDemo/mew_baseline.jpg",
-    "https://via.placeholder.com/200x200.jpg",
-    "https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_5.jpg",
-    "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/w3c.svg",
-    "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/wikimedia.svg",
-    "https://raw.githubusercontent.com/icons8/flat-color-icons/master/pdf/stack_of_photos.pdf",
-    "https://raw.githubusercontent.com/icons8/flat-color-icons/master/pdf/smartphone_tablet.pdf"
+        "http://assets.sbnation.com/assets/2512203/dogflops.gif",
+        "https://raw.githubusercontent.com/liyong03/YLGIFImage/master/YLGIFImageDemo/YLGIFImageDemo/joy.gif",
+        "http://apng.onevcat.com/assets/elephant.png",
+        "http://www.ioncannon.net/wp-content/uploads/2011/06/test2.webp",
+        "http://www.ioncannon.net/wp-content/uploads/2011/06/test9.webp",
+        "http://littlesvr.ca/apng/images/SteamEngine.webp",
+        "http://littlesvr.ca/apng/images/world-cup-2014-42.webp",
+        "https://isparta.github.io/compare-webp/image/gif_webp/webp/2.webp",
+        "https://raw.githubusercontent.com/link-u/avif-sample-images/master/fox.profile0.8bpc.yuv420.avif",
+        "https://raw.githubusercontent.com/link-u/avif-sample-images/master/star-12bpc-with-alpha.avifs",
+        "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic",
+        "https://nokiatech.github.io/heif/content/image_sequences/starfield_animation.heic",
+        "https://nr-platform.s3.amazonaws.com/uploads/platform/published_extension/branding_icon/275/AmazonS3.png",
+        "https://raw.githubusercontent.com/ibireme/YYImage/master/Demo/YYImageDemo/mew_baseline.jpg",
+        "https://via.placeholder.com/200x200.jpg",
+        "https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_5.jpg",
+        "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/w3c.svg",
+        "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/wikimedia.svg",
+        "https://raw.githubusercontent.com/icons8/flat-color-icons/master/pdf/stack_of_photos.pdf",
+        "https://raw.githubusercontent.com/icons8/flat-color-icons/master/pdf/smartphone_tablet.pdf"
     ]
     @State var animated: Bool = false // You can change between WebImage/AnimatedImage
     @EnvironmentObject var settings: UserSettings
-    
+
     // Used to avoid https://twitter.com/fatbobman/status/1572507700436807683?s=20&t=5rfj6BUza5Jii-ynQatCFA
     struct ItemView: View {
         @Binding var animated: Bool
@@ -142,32 +167,32 @@ struct ContentView: View {
                 HStack {
                     if self.animated {
                         #if os(macOS) || os(iOS) || os(tvOS) || os(visionOS)
-                        AnimatedImage(url: URL(string:url))
-                        .onViewUpdate { view, context in
-                        #if os(macOS)
-                            view.toolTip = url
-                        #endif
-                        }
-                        .indicator(.activity)
-                        .transition(.fade)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
+                        AnimatedImage(url: URL(string: url))
+                            .onViewUpdate { view, _ in
+                                #if os(macOS)
+                                view.toolTip = url
+                                #endif
+                            }
+                            .indicator(.activity)
+                            .transition(.fade)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
                         #else
-                        WebImage(url: URL(string:url))
-                        .resizable()
-                        .indicator(.activity)
-                        .transition(.fade(duration: 0.5))
-                        .scaledToFit()
-                        .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
+                        WebImage(url: URL(string: url))
+                            .resizable()
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                            .scaledToFit()
+                            .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
                         #endif
                     } else {
-                        WebImage(url: URL(string:url))
-                        .resizable()
-                        .indicator(.activity)
-                        .transition(.fade(duration: 0.5))
-                        .scaledToFit()
-                        .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
+                        WebImage(url: URL(string: url))
+                            .resizable()
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                            .scaledToFit()
+                            .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
                     }
                     Text((url as NSString).lastPathComponent)
                 }
@@ -176,64 +201,60 @@ struct ContentView: View {
         }
     }
 
-    
     var body: some View {
         #if os(visionOS)
         return NavigationView {
             contentView()
-            .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
-            .navigationBarItems(leading:
-                Button(action: { self.reloadCache() }) {
-                    Text("Reload")
-                }, trailing:
-                Button(action: { self.switchView() }) {
-                    Text("Switch")
-                }
-            )
+                .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
+                .navigationBarItems(leading:
+                    Button(action: { self.reloadCache() }) {
+                        Text("Reload")
+                    }, trailing:
+                    Button(action: { self.switchView() }) {
+                        Text("Switch")
+                    })
         }
         #endif
         #if os(iOS)
         return NavigationView {
             contentView()
-            .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
-            .navigationBarItems(leading:
-                Button(action: { self.reloadCache() }) {
-                    Text("Reload")
-                }, trailing:
-                Button(action: { self.switchView() }) {
-                    Text("Switch")
-                }
-            )
+                .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
+                .navigationBarItems(leading:
+                    Button(action: { self.reloadCache() }) {
+                        Text("Reload")
+                    }, trailing:
+                    Button(action: { self.switchView() }) {
+                        Text("Switch")
+                    })
         }
         #endif
         #if os(tvOS)
         return NavigationView {
             contentView()
-            .environment(\EnvironmentValues.editMode, self.$settings.editMode)
-            .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
-            .navigationBarItems(leading:
-                Button(action: { self.reloadCache() }) {
-                    Text("Reload")
-                }, trailing:
-                Button(action: { self.switchView() }) {
-                    Text("Switch")
-                }
-            )
+                .environment(\EnvironmentValues.editMode, self.$settings.editMode)
+                .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
+                .navigationBarItems(leading:
+                    Button(action: { self.reloadCache() }) {
+                        Text("Reload")
+                    }, trailing:
+                    Button(action: { self.switchView() }) {
+                        Text("Switch")
+                    })
         }
         #endif
         #if os(macOS)
         return NavigationView {
             contentView()
-            .frame(minWidth: 200)
-            .listStyle(SidebarListStyle())
-            .contextMenu {
-                Button(action: { self.reloadCache() }) {
-                    Text("Reload")
+                .frame(minWidth: 200)
+                .listStyle(SidebarListStyle())
+                .contextMenu {
+                    Button(action: { self.reloadCache() }) {
+                        Text("Reload")
+                    }
+                    Button(action: { self.switchView() }) {
+                        Text("Switch")
+                    }
                 }
-                Button(action: { self.switchView() }) {
-                    Text("Switch")
-                }
-            }
         }
         #endif
         #if os(watchOS)
@@ -245,13 +266,13 @@ struct ContentView: View {
                         Text("Reload")
                     }
                 }
-            
         }
         #endif
     }
-    
+
     func contentView() -> some View {
         List {
+            FastSwitchGifView(fileName: "joy")
             ForEach(imageURLs, id: \.self) { url in
                 // Must use top level view instead of inlined view structure
                 ItemView(animated: $animated, url: url)
@@ -263,12 +284,12 @@ struct ContentView: View {
             }
         }
     }
-    
+
     func reloadCache() {
         SDImageCache.shared.clearMemory()
         SDImageCache.shared.clearDisk(onCompletion: nil)
     }
-    
+
     func switchView() {
         SDImageCache.shared.clearMemory()
         animated.toggle()
